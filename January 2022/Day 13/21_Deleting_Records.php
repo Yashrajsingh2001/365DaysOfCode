d4a1276b34bf9ac6fe64ccdeb0b8a79f385d01bf
Yashraj Singh Boparai <yashrajsingh.boparai@gmail.com>
2022-01-02 21:03:57 +0530

    day 13: php

---
+++ January 2022/Day 13/21_Deleting_Records.php
@@ -0,0 +1,33 @@
+<?php
+// Connecting to the Database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "yashrajphp";
+
+// Create a connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Die if connection was not successful
+if (!$conn){
+    die("Sorry we failed to connect: ". mysqli_connect_error());
+}
+else{
+    echo "Connection was successful<br>";
+}
+
+
+$sql = "DELETE FROM `phptrip` WHERE `dest` = 'Canada' LIMIT 5"; // Limit 5 = delete 5 elements
+$result = mysqli_query($conn, $sql);
+$aff = mysqli_affected_rows($conn);
+echo "<br>Number of affected rows: $aff <br>";
+
+if($result){
+    echo "Delete successfully";
+}
+else{
+    $err = mysqli_error($conn);
+    echo "Not Delete successfully due to this error --> $err";
+}
+
+?>
