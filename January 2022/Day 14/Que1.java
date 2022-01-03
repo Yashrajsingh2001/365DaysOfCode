553ed757d9fe6529db8845ef1bca97e80e007ccb
Yashraj Singh Boparai <yashrajsingh.boparai@gmail.com>
2022-01-03 11:47:57 +0530

    day 14 que 1

---
+++ January 2022/Day 14/Que1.java
@@ -0,0 +1,15 @@
+// Que LINK: https://leetcode.com/problems/find-the-town-judge/
+
+class Solution {
+    public int findJudge(int N, int[][] trust) {
+        int[] count = new int[N+1];
+        for (int[] t: trust) {
+            count[t[0]]--;
+            count[t[1]]++;
+        }
+        for (int i = 1; i <= N; ++i) {
+            if (count[i] == N - 1) return i;
+        }
+        return -1;
+    }
+}
