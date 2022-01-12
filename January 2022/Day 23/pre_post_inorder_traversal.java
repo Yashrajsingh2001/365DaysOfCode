1050eb9d4f2fc4f1ca5a8180dad72ea7f28f6841
Yashraj Singh Boparai <yashrajsingh.boparai@gmail.com>
2022-01-12 20:01:30 +0530

    que 4

--- January 2022/Day 23/pre_post_inorder_traversal.java
+++ January 2022/Day 23/pre_post_inorder_traversal.java
@@ -43,3 +43,71 @@ public void postorder(TreeNode root, List<Integer> nums) {
 	postorder(root.right, nums);
 	nums.add(root.val);
 }
+
+
+//* *************************************************************************************************************************
+// Credit: https://leetcode.com/problems/binary-tree-postorder-traversal/discuss/45551/Preorder-Inorder-and-Postorder-Iteratively-Summarization
+
+
+// Preorder: Node -> Left -> Right
+// Pre Order Traverse
+public List<Integer> preorderTraversal(TreeNode root) {
+    List<Integer> result = new ArrayList<>();
+    Deque<TreeNode> stack = new ArrayDeque<>();
+    TreeNode p = root;
+    while(!stack.isEmpty() || p != null) {
+        if(p != null) {
+            stack.push(p);
+            result.add(p.val);  // Add before going to children
+            p = p.left;
+        } else {
+            TreeNode node = stack.pop();
+            p = node.right;   
+        }
+    }
+    return result;
+}
+
+
+
+
+// Inorder : Left -> Node -> Right
+// In Order Traverse
+public List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> result = new ArrayList<>();
+    Deque<TreeNode> stack = new ArrayDeque<>();
+    TreeNode p = root;
+    while(!stack.isEmpty() || p != null) {
+        if(p != null) {
+            stack.push(p);
+            p = p.left;
+        } else {
+            TreeNode node = stack.pop();
+            result.add(node.val);  // Add after all left children
+            p = node.right;   
+        }
+    }
+    return result;
+}
+
+
+
+
+// Postorder : Left -> Right -> Node
+// Post Order Traverse
+public List<Integer> postorderTraversal(TreeNode root) {
+    LinkedList<Integer> result = new LinkedList<>();
+    Deque<TreeNode> stack = new ArrayDeque<>();
+    TreeNode p = root;
+    while(!stack.isEmpty() || p != null) {
+        if(p != null) {
+            stack.push(p);
+            result.addFirst(p.val);  // Reverse the process of preorder
+            p = p.right;             // Reverse the process of preorder
+        } else {
+            TreeNode node = stack.pop();
+            p = node.left;           // Reverse the process of preorder
+        }
+    }
+    return result;
+}
