d0b70a4a5e805c9cdc757fc9ccedca647298eaf3
Yashraj Singh Boparai <yashrajsingh.boparai@gmail.com>
2022-01-16 22:04:54 +0530

    day 27: solved 4 que

---
+++ January 2022/Day 27/Que3.java
@@ -0,0 +1,41 @@
+// Que link: https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
+// 653. Two Sum IV - Input is a BST
+
+
+// Method 1:
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        return dfs(root, set, k);
+    }
+
+    public boolean dfs(TreeNode root, HashSet<Integer> set, int k){
+        if(root == null)return false;
+        if(set.contains(k - root.val))return true;
+        set.add(root.val);
+        return dfs(root.left, set, k) || dfs(root.right, set, k);
+    }
+}
+
+
+
+// Method 2:
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        List<Integer> nums = new ArrayList<>();
+        inorder(root, nums);
+        for(int i = 0, j = nums.size()-1; i<j;){
+            if(nums.get(i) + nums.get(j) == k)return true;
+            if(nums.get(i) + nums.get(j) < k)i++;
+            else j--;
+        }
+        return false;
+    }
+
+    public void inorder(TreeNode root, List<Integer> nums){
+        if(root == null)return;
+        inorder(root.left, nums);
+        nums.add(root.val);
+        inorder(root.right, nums);
+    }
+}
