27caa35932dd724494cf748a32bbf65d27262321
Yashraj Singh Boparai <yashrajsingh.boparai@gmail.com>
2022-01-17 16:13:48 +0530

    Problem Code: DEVSPORTS

---
+++ January 2022/Day 28/Que7.java
@@ -0,0 +1,28 @@
+// https://www.codechef.com/problems/DEVSPORTS
+// Problem Code: DEVSPORTS
+
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner scan = new Scanner(System.in);
+        int T = scan.nextInt();
+        while(T-- != 0){
+            int Z = scan.nextInt();
+            int Y = scan.nextInt();
+            int A = scan.nextInt();
+            int B = scan.nextInt();
+            int C = scan.nextInt();
+            System.out.println(Z-Y-A-B-C>=0?"YES":"NO");
+        }
+	}
+}
