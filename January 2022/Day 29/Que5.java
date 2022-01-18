473b047a4c839e56357100f0157b4cd8252cc408
Yashraj Singh Boparai <yashrajsingh.boparai@gmail.com>
2022-01-18 15:59:22 +0530

    Problem Code: HARDBET

---
+++ January 2022/Day 29/Que5.java
@@ -0,0 +1,26 @@
+// https://www.codechef.com/problems/HARDBET
+// Problem Code: HARDBET
+
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner scan = new Scanner(System.in);
+        int T = scan.nextInt();
+        while(T-- != 0){
+            int A = scan.nextInt();
+            int B = scan.nextInt();
+            int C = scan.nextInt();
+            System.out.println(C<B && C<A? "Alice" : B<C && B<A? "Bob" : "Draw");
+        }
+	}
+}
