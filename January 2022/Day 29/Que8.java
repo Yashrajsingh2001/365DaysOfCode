74ffd6d25b1c88c3884d855949d2f8d998d8325d
Yashraj Singh Boparai <yashrajsingh.boparai@gmail.com>
2022-01-18 17:21:39 +0530

    Problem Code: BLACKCEL

---
+++ January 2022/Day 29/Que8.java
@@ -0,0 +1,20 @@
+// https://www.codechef.com/problems/BLACKCEL
+// Problem Code: BLACKCEL
+
+
+/* package codechef; // don't place package name! */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println(n*n/2);
+	}
+}
