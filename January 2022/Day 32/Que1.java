4aa6c93e4b2439466a2ec896319dee673dad1998
Yashraj Singh Boparai <yashrajsingh.boparai@gmail.com>
2022-01-21 18:35:29 +0530

    did 2 solution

--- January 2022/Day 32/Que1.java
+++ January 2022/Day 32/Que1.java
@@ -2,41 +2,10 @@
 // 134. Gas Station
 
 
-
+// Method 1: Easy to understand
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        //determine if we have a solution
-        int total = 0;
-        for (int i = 0; i < gas.length; i++) {
-            total += gas[i] - cost[i];
-        }
-        if (total < 0) {
-            return -1;
-        }
-   
-        // find out where to start
-        int tank = 0;
-        int start = 0;
-        for (int i = 0; i < gas.length;i++) {
-            tank += gas[i] - cost[i];
-            if (tank < 0) {
-                start = i + 1;
-                tank = 0;
-            }
-        }
-        return start;
-    }
-}
-
-
-
-
-class Solution {
-    public int canCompleteCircuit(int[] gas, int[] cost) {
-        int sumGas = 0;
-        int sumCost = 0;
-        int start = 0;
-        int tank = 0;
+        int sumGas = 0, sumCost = 0, start = 0, tank = 0;
         for (int i = 0; i < gas.length; i++) {
             sumGas += gas[i];
             sumCost += cost[i];
@@ -56,6 +25,8 @@ class Solution {
 
 
 
+
+// Method 2:
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int tank=0, total=0, start=0;
