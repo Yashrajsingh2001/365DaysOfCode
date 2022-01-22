6f1c1cb0ac99cc244a6b4aae0a70bdd5d268c399
Yashraj Singh Boparai <yashrajsingh.boparai@gmail.com>
2022-01-22 15:16:50 +0530

    add comments

--- January 2022/Day 33/Que1.java
+++ January 2022/Day 33/Que1.java
@@ -20,3 +20,23 @@ class Que1 {
         System.out.println(winnerSquareGame(4));
     }
 }
+
+
+/*
+//* For LeetCode submission:
+class Solution {
+    public boolean winnerSquareGame(int n) {
+        boolean[] dp = new boolean[n + 1];
+        for (int i = 1; i <= n; ++i) {
+            for (int k = 1; k * k <= i; ++k) {
+                if (!dp[i - k * k]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
+
+*/
