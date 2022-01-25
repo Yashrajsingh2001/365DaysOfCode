6fa7af3a596a15189774588b4e1182ae5ffdd33f
Yashraj Singh Boparai <yashrajsingh.boparai@gmail.com>
2022-01-25 23:07:15 +0530

    941. Valid Mountain Array

--- January 2022/Day 36/Que1.java
+++ January 2022/Day 36/Que1.java
@@ -5,15 +5,15 @@
 // Method 1:
 class Solution {
     public boolean validMountainArray(int[] A) {
-      // if (A.length < 3) return false;
-      int start = 0;
-      int end = A.length-1;
-      while (start < end) {
-        if (A[start+1] > A[start]) start++;
-        else if (A[end-1] > A[end]) end--;
-        else break;
-      }
-      return start != 0 && end != A.length-1 && start == end;
+      	// if (A.length < 3) return false;
+		int start = 0;
+		int end = A.length-1;
+		while (start < end) {
+			if (A[start+1] > A[start]) start++;
+			else if (A[end-1] > A[end]) end--;
+			else break;
+		}
+		return start != 0 && end != A.length-1 && start == end;
     }
 }
 
