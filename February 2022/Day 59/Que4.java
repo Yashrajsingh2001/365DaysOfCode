c9f2709233948607e6a0c4faf2f5793ff8d54a52
Yashraj Singh Boparai <yashrajsingh.boparai@gmail.com>
2022-02-17 22:59:19 +0530

    377. Combination Sum IV

---
+++ February 2022/Day 59/Que4.java
@@ -0,0 +1,18 @@
+// Que link: https://leetcode.com/problems/combination-sum-iv/
+// 377. Combination Sum IV
+
+
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target+1];
+        dp[0] = 1;
+        for(int i = 1; i <= target; i++){
+            for(int num: nums){
+                if(num <= i){
+                    dp[i] += dp[i-num];
+                }
+            }
+        }
+        return dp[target];
+    }
+}
--- February 2022/Day 59/README.md
+++ February 2022/Day 59/README.md
@@ -5,3 +5,4 @@
 | 1 | LeetCode | [39. Combination Sum](https://leetcode.com/problems/combination-sum/) |
 | 2 | LeetCode | [40. Combination Sum II](https://leetcode.com/problems/combination-sum-ii/) |
 | 3 | LeetCode | [216. Combination Sum III](https://leetcode.com/problems/combination-sum-iii/) |
+| 4 | LeetCode | [377. Combination Sum IV](https://leetcode.com/problems/combination-sum-iv/) |
