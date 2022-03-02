3b3cf8a0648619550321dff39b6a36ff461ddd4c
Yashraj Singh Boparai <yashrajsingh.boparai@gmail.com>
2022-03-02 20:03:31 +0530

    392. Is Subsequence

--- March 2022/Day 72/Que1.java
+++ March 2022/Day 72/Que1.java
@@ -4,21 +4,15 @@
 
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        List<Integer>[] idx = new List[256]; // Just for clarity
-        for (int i = 0; i < t.length(); i++) {
-            if (idx[t.charAt(i)] == null)
-                idx[t.charAt(i)] = new ArrayList<>();
-            idx[t.charAt(i)].add(i);
+        if (s.length() == 0) return true;
+        int indexS = 0, indexT = 0;
+        while (indexT < t.length()) {
+            if (t.charAt(indexT) == s.charAt(indexS)) {
+                indexS++;
+                if (indexS == s.length()) return true;
+            }
+            indexT++;
         }
-        
-        int prev = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (idx[s.charAt(i)] == null) return false; // Note: char of S does NOT exist in T causing NPE
-            int j = Collections.binarySearch(idx[s.charAt(i)], prev);
-            if (j < 0) j = -j - 1;
-            if (j == idx[s.charAt(i)].size()) return false;
-            prev = idx[s.charAt(i)].get(j) + 1;
-        }
-        return true;
+        return false;
     }
 }
