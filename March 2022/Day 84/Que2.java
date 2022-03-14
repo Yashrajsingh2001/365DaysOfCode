99c47cfd521e7db36b347829c38cc5673c178fac
Yashraj Singh Boparai <yashrajsingh.boparai@gmail.com>
2022-03-14 22:11:37 +0530

    90. Subsets II

---
+++ March 2022/Day 84/Que2.java
@@ -0,0 +1,23 @@
+// Que link: https://leetcode.com/problems/subsets-ii/
+// 90. Subsets II
+
+
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        result.add(new ArrayList<Integer>());
+        int begin = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(i == 0 || nums[i] != nums[i - 1]) begin = 0;
+            int size = result.size();
+            for(int j = begin; j < size; j++){
+                List<Integer> cur = new ArrayList<Integer>(result.get(j));
+                cur.add(nums[i]);
+                result.add(cur);
+            }
+            begin = size;
+        }
+        return result;
+    }
+}
--- March 2022/Day 84/README.md
+++ March 2022/Day 84/README.md
@@ -3,3 +3,4 @@
 | Que No. | From | Description |
 | --- | --- | --- |
 | 1 | LeetCode | [71. Simplify Path](https://leetcode.com/problems/simplify-path/) |
+| 2 | LeetCode | [90. Subsets II](https://leetcode.com/problems/subsets-ii/) |
