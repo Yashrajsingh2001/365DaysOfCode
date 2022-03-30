4a466a430cb877f4496bea8fb9ad3cae577c7f81
Yashraj Singh Boparai <yashrajsingh.boparai@gmail.com>
2022-03-30 22:35:03 +0530

    81. Search in Rotated Sorted Array II

--- March 2022/Day 88/Que1.java
+++ March 2022/Day 88/Que1.java
@@ -2,6 +2,7 @@
 // 81. Search in Rotated Sorted Array II
 
 
+// Method 1:
 class Solution {
     public boolean search(int[] nums, int target) {
         int start=0, end=nums.length-1;
@@ -25,6 +26,48 @@ class Solution {
             }else{
                 start ++;
             }
+        }
+        return false;
+    }
+}
+
+
+// Method 2:
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int left=0, right=nums.length-1;
+        
+        while(left<=right){
+            if(nums[left]==target || nums[right]==target) return true;
+            else if(target > nums[left]) left++;
+            else if(target < nums[right]) right--;
+            else break;
+        }
+        return false;
+    }
+}
+
+
+// Method 3:
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int left=0, right=nums.length-1;
+        
+        while(left<=right){
+            if(nums[left]==target || nums[right]==target) return true;
+            else if(target > nums[left]){
+                while(left<right && nums[left+1]==nums[left]){
+                    left++;
+                }
+                left++;   
+            }
+            else if(target < nums[right]) {
+                while(left<right && nums[right-1]==nums[right]){
+                    right--;
+                }
+                right--;   
+            }
+            else break;
         }
         return false;
     }
