d54fbbfb8044d90779e3c32b7ba712cfb0cbe1ee
Yashraj Singh Boparai <yashrajsingh.boparai@gmail.com>
2022-03-31 14:16:16 +0530

    A. Mahmoud and Ehab and the even-odd game

---
+++ March 2022/Day 89/Fafa_and_his_Company.java
@@ -0,0 +1,16 @@
+// Que link: https://codeforces.com/problemset/problem/935/A
+A. Fafa and his Company
+
+
+import java.util.*;
+
+public class Fafa_and_his_Company{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int ans=0;
+        for(int i=1; i<=num/2; i++)
+            if(num%i==0) ans++;
+        System.out.println(ans);
+    }
+}
