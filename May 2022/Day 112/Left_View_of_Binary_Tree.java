30fd5d3fa4887e7d5373d9029f839ae3babfab9d
Yashraj Singh Boparai <yashrajsingh.boparai@gmail.com>
2022-05-08 19:59:33 +0530

    Left View of Binary Tree

---
+++ May 2022/Day 112/Left_View_of_Binary_Tree.java
@@ -0,0 +1,30 @@
+// Que link: https://practice.geeksforgeeks.org/problems/left-view-of-binary-tree/1#
+// Left View of Binary Tree
+
+
+class Tree {
+    // Function to return list containing elements of left view of binary tree.
+    ArrayList<Integer> leftView(Node root) {
+        // Your code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        if (root == null)
+            return ans;
+
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            ans.add(q.peek().data);
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+                Node curr = q.poll();
+                if (curr.left != null)
+                    q.offer(curr.left);
+                if (curr.right != null)
+                    q.offer(curr.right);
+            }
+        }
+        return ans;
+    }
+}
--- May 2022/Day 112/README.md
+++ May 2022/Day 112/README.md
@@ -3,3 +3,4 @@
 | Que No. | From | Description |
 | --- | --- | --- |
 | 1 | CodeForces | [B. Badges](https://codeforces.com/problemset/problem/1214/B) |
+| 1 | GFG | [Left View of Binary Tree](https://practice.geeksforgeeks.org/problems/left-view-of-binary-tree/1#) |
